33d0edb515ab6b336f132f6127631e87ed406e09
charmaine <charmaine.rabano@gmail.com>
2022-01-28 20:04:09 +0800

    implemented librarian interface ui

--- tailwind.config.js
+++ tailwind.config.js
@@ -8,17 +8,20 @@
     theme: {
         extend: {
             colors: {
-                'darkblue': '#022859',
-                'ashblue': '#657A8C',
-                'darkgray':'#a6a6a6',
-                'lightgray': '#F2F2F2',
-                'black': '#0d0d0d',
-                'red': '#e74141',
-                'orange': '#E4984E',
-                'yellow': '#e2e140',
-                'lightgreen': '#73df44',
-                'green':'#46dd60'
-            }
+                'custom-darkblue': '#022859',
+                'custom-ashblue': '#657A8C',
+                'custom-darkgray':'#a6a6a6',
+                'custom-lightgray': '#F2F2F2',
+                'custom-black': '#0d0d0d',
+                'custom-red': '#e74141',
+                'custom-orange': '#E4984E',
+                'custom-yellow': '#e2e140',
+                'custom-lightgreen': '#73df44',
+                'custom-green':'#46dd60'
+            },
+            fontFamily: {
+                sans: ['Nunito', 'sans-serif'],
+            },
         },
     },
     variants: {
